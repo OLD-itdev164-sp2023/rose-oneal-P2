dd464367bf361c4f08f8268d6c1185c15418aaa9
onealr4 <onealr4@gmatc.matc.edu>
2023-05-12 19:14:01 -0500

    adding video

---
+++ product-review/src/components/Hero.js
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+import { Button } from "./Button";
+
+const Hero = () => {
+    return (
+        <HeroContanier>
+            <HeroBg>
+                <VideoBg src="" type="video/mp4" />
+            </HeroBg>
+            <HeroContent>
+                <HeroItems>
+                    <HeroH1>Gaming Reimagined</HeroH1>
+                    <HeroP>Out of this world</HeroP>
+                    <Button>Buy Now</Button>
+                </HeroItems>
+            </HeroContent>
+        </HeroContanier>
+    )
+}
+
+export default Hero
+
+const HeroContanier = styled.div`
+    background: #0c0c0c;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    padding: 0 1rem;
+    position: relative;
+    margin-top: -80px; //whenever the background is transparent in the header this should be the same as the nav bar height but negative
+    color: #fff;
+`
+
+const HeroBg = styled.div``
+
+const VideoBg = styled.video``
+
+const HeroContent = styled.div``
+const HeroItems = styled.div``
+const HeroH1 = styled.h1``
+const HeroP = styled.p``
--- product-review/src/components/header.js
+++ product-review/src/components/header.js
@@ -29,7 +29,7 @@ export default Header
 
 //styling the nav
 const Nav = styled.nav`
-  background: red;
+  background: transparent;
   height: 80px;
   display: flex;
   justify-content: space-between;
